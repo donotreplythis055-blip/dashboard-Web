4914d6301a6f2b3f1cd9f4ef4dda05b83ffab4eb
donotreplythis055-blip <donotreplythis055@gmail.com>
2026-04-11 03:57:28 +0200

    Update index.php

--- dashboard2/fd/index.php
+++ dashboard2/fd/index.php
@@ -1,14 +1,14 @@
 <?php
-session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+echo "ELINDULT";
+
+// session_start();  👈 ezt most kommenteld ki!
 
 // Session ellenőrzés
-if (isset($_SESSION['user_id'])) {
-    header('Location: dashboard2\fd\index.html');
-    exit();
-}
+//if (isset($_SESSION['user_id'])) {
+//    header('Location: dashboard2\fd\index.html');
+ //   exit();
+//}
 ?>
