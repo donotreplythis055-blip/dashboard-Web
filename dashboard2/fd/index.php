9562d8c4b3759e34beeb5749a776e4efd26cecd7
donotreplythis055-blip <donotreplythis055@gmail.com>
2026-04-11 04:01:49 +0200

    Update index.php

--- dashboard2/fd/index.php
+++ dashboard2/fd/index.php
@@ -1,14 +1,20 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-echo "ELINDULT";
-
-// session_start();  👈 ezt most kommenteld ki!
-
-// Session ellenőrzés
-//if (isset($_SESSION['user_id'])) {
-//    header('Location: dashboard2\fd\index.html');
- //   exit();
-//}
+session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/dashboard2/fd/fd.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Főoldal</title>
+</head>
+<body>
+    <h1>Üdvözöljük</h1>
+    <p>Kérjük, jelentkezzen be vagy regisztráljon.</p>
+
+    <a href="/dashboard2/bej/login.php">Bejelentkezés</a>
+    <a href="/dashboard2/reg/create_user.php">Regisztráció</a>
+</body>
+</html>
