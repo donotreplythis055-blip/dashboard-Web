ca9a8305fe1c7f01c8924aeb0832ae6255667226
donotreplythis055-blip <donotreplythis055@gmail.com>
2026-04-11 03:51:51 +0200

    Fix index.php

--- dashboard2/fd/index.php
+++ dashboard2/fd/index.php
@@ -6,9 +6,6 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// a többi kód...
-session_start();
-
 // Session ellenőrzés
 if (isset($_SESSION['user_id'])) {
     header('Location: dashboard2\fd\index.html');
