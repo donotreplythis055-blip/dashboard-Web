0c29c1cb5bd115cd397640979909113029c5ad76
donotreplythis055-blip <donotreplythis055@gmail.com>
2026-04-11 04:30:17 +0200

    Update init.php

--- init.php
+++ init.php
@@ -4,7 +4,7 @@ try {
     $pdo = new PDO(
         "pgsql:host=dpg-d7cqqn7lk1mc73ebedug-a.oregon-postgres.render.com;port=5432;dbname=dashboard_db_x12n",
         "dashboard_db_x12n_user",
-        "A_SAJAT_JELSZAVAD"
+        "5OiwanCxna3FJaYiIvdiV80yy7dnD3UT"
     );
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
