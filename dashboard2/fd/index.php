f89ca5d653d678c61d707e7151e1632be20ec1b9
donotreplythis055-blip <donotreplythis055@gmail.com>
2026-04-11 03:48:48 +0200

    Fix index.php

--- dashboard2/fd/index.php
+++ dashboard2/fd/index.php
@@ -3,4 +3,16 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require __DIR__ . '/dashboard2/fd/index.php';
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// a többi kód...
+session_start();
+
+// Session ellenőrzés
+if (isset($_SESSION['user_id'])) {
+    header('Location: dashboard2\fd\index.html');
+    exit();
+}
+?>
