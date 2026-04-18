90fde5af5a7b54100c7a604d14aaf9f839c6e214
donotreplythis055-blip <donotreplythis055@gmail.com>
2026-04-18 11:15:04 +0200

    Add files via upload

---
+++ dashboard2/db.php
@@ -0,0 +1,9 @@
+<?php
+$pdo = new PDO(
+    "pgsql:host=db.souzzuluszoophkkjlhv.supabase.co;port=5432;dbname=postgres",
+    "postgres",
+    "XXZ/*+JqsaYxQ2$"
+);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
